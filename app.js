c1aca31a4a2e0027baa30bc77eb0bd933b25c8a5
haya2001a <hayaalami0@gmail.com>
2022-09-19 21:35:53 +0300

    adding the first task using html & css & js

---
+++ app.js
@@ -0,0 +1,6 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+var w1=screen.width;
+var h1= screen.height;
+var x = document.getElementById("demo");
+x.innerHTML = "ابعاد شاشتك الحالية : " + w1 + " عرض , " + h1 + "  ارتفاع<br>ابعاد النافذة الحالية : " + w + " عرض , " + h + "  ارتفاع" ;
